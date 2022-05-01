fa4847b1d6cdcca7c1d2a68336490640c6b634fd
Luke Horvat <lukehorvat@gmail.com>
2022-05-01 14:54:46 +0200

    Make player list responsive

--- el-players-online-enhancer/index.user.js
+++ el-players-online-enhancer/index.user.js
@@ -25,6 +25,12 @@ function initHead() {
   favicon.href = 'http://www.eternal-lands.com/favicon.ico';
   document.head.appendChild(favicon);
 
+  // Add a viewport meta tag so that media queries work nicely.
+  const viewport = document.createElement('meta');
+  viewport.name = 'viewport';
+  viewport.content = 'width=device-width,initial-scale=1';
+  document.head.appendChild(viewport);
+
   const style = document.createElement('style');
   style.innerHTML = `
     body {
@@ -37,23 +43,53 @@ function initHead() {
       font-size: 18px;
     }
 
-    select {
+    input + select {
       margin-left: 10px;
     }
 
     ul {
       display: grid;
-      grid-template-columns: repeat(5, 1fr);
-      grid-gap: 10px;
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 15px;
       margin: 0;
       padding: 0;
       list-style-type: none;
     }
 
+    @media (min-width: 400px) {
+      ul {
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+
+    @media (min-width: 600px) {
+      ul {
+        grid-template-columns: repeat(4, 1fr);
+      }
+    }
+
+    @media (min-width: 800px) {
+      ul {
+        grid-template-columns: repeat(5, 1fr);
+      }
+    }
+
+    @media (min-width: 1000px) {
+      ul {
+        grid-template-columns: repeat(6, 1fr);
+      }
+    }
+
+    @media (min-width: 1200px) {
+      ul {
+        grid-template-columns: repeat(7, 1fr);
+      }
+    }
+
     ul li a {
       padding: 3px 5px;
       background: #555;
-      font-size: 12px;
+      font-size: 13px;
       font-family: monospace;
       text-decoration: none;
       border-radius: 4px;
