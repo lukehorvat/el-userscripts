2c87513a95c8ad08af00d0564aaf37001902a574
Luke Horvat <lukehorvat@gmail.com>
2023-03-02 22:25:32 +0100

    Include output directory

---
+++ dist/map-sizes.js
@@ -0,0 +1,58 @@
+window.mapSizes = {
+  "startmap": {
+    "width": 192,
+    "height": 192
+  },
+  "map9f": {
+    "width": 384,
+    "height": 384
+  },
+  "map8": {
+    "width": 192,
+    "height": 192
+  },
+  "map7": {
+    "width": 384,
+    "height": 384
+  },
+  "map6nf": {
+    "width": 384,
+    "height": 384
+  },
+  "map5nf": {
+    "width": 192,
+    "height": 192
+  },
+  "map4f": {
+    "width": 192,
+    "height": 192
+  },
+  "map3": {
+    "width": 384,
+    "height": 384
+  },
+  "map2": {
+    "width": 768,
+    "height": 768
+  },
+  "map15f": {
+    "width": 384,
+    "height": 384
+  },
+  "map14f": {
+    "width": 192,
+    "height": 192
+  },
+  "map13": {
+    "width": 192,
+    "height": 192
+  },
+  "map12": {
+    "width": 192,
+    "height": 192
+  },
+  "map11": {
+    "width": 384,
+    "height": 384
+  }
+};
