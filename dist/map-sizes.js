3b23e805ddf2e520ab851698d569347df2ec0cc9
Luke Horvat <lukehorvat@gmail.com>
2023-03-05 01:40:46 +0100

    Add more maps

--- dist/map-sizes.js
+++ dist/map-sizes.js
@@ -54,5 +54,113 @@ window.mapSizes = {
   "map11": {
     "width": 384,
     "height": 384
+  },
+  "cont2map9": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map8": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map7": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map6": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map5": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map4": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map3": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map24": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map23": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map22": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map21": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map20": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map2": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map19": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map18": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map17": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map16_insides": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map16": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map15": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map14": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map13": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map12": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map11": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map10": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map1": {
+    "width": 768,
+    "height": 768
+  },
+  "cave1": {
+    "width": 192,
+    "height": 192
+  },
+  "anitora": {
+    "width": 384,
+    "height": 384
   }
 };
