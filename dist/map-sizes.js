f5e68fa779072dd8d065cfc0e0ce67d26dce5ba5
Luke Horvat <lukehorvat@gmail.com>
2023-07-23 14:50:27 +0200

    Replace .png files with .dds and regenerate map images

--- dist/map-sizes.js
+++ dist/map-sizes.js
@@ -3,9 +3,33 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "c2_docks_irillion": {
+    "width": 96,
+    "height": 96
+  },
+  "c2_docks_seridia": {
+    "width": 96,
+    "height": 96
+  },
+  "c2_portalsroom": {
+    "width": 192,
+    "height": 192
+  },
+  "c2_underworld": {
+    "width": 192,
+    "height": 192
+  },
   "cave1": {
     "width": 192,
     "height": 192
+  },
+  "cave2": {
+    "width": 96,
+    "height": 96
+  },
+  "cave3": {
+    "width": 96,
+    "height": 96
   },
   "cont2map1": {
     "width": 768,
@@ -15,7 +39,23 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map10_caves": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map10_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map11": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map11_cave1": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map11_insides": {
     "width": 384,
     "height": 384
   },
@@ -23,15 +63,55 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map12_cave1": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map12_cave2": {
+    "width": 96,
+    "height": 96
+  },
+  "cont2map12_cave3": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map12_fortress": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map12_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map13": {
     "width": 192,
     "height": 192
+  },
+  "cont2map13_temple": {
+    "width": 96,
+    "height": 96
   },
   "cont2map14": {
     "width": 384,
     "height": 384
   },
+  "cont2map14_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map15": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map15_caves": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map15_insides": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map15_mountain": {
     "width": 384,
     "height": 384
   },
@@ -43,7 +123,19 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map16_mountains": {
+    "width": 192,
+    "height": 192
+  },
   "cont2map17": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map17_crypt": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map17_insides": {
     "width": 192,
     "height": 192
   },
@@ -51,9 +143,29 @@ window.mapSizes = {
     "width": 192,
     "height": 192
   },
+  "cont2map18_caves": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map18_insides": {
+    "width": 192,
+    "height": 192
+  },
   "cont2map19": {
     "width": 192,
     "height": 192
+  },
+  "cont2map1_caves": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map1_insides": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map1_mine": {
+    "width": 384,
+    "height": 384
   },
   "cont2map2": {
     "width": 192,
@@ -63,7 +175,39 @@ window.mapSizes = {
     "width": 192,
     "height": 192
   },
+  "cont2map20_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map21": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map21_caves": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze1": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze2": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze3": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze4": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze5": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map21_maze6": {
     "width": 384,
     "height": 384
   },
@@ -71,7 +215,19 @@ window.mapSizes = {
     "width": 768,
     "height": 768
   },
+  "cont2map22_caves": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map22_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map23": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map23_insides": {
     "width": 384,
     "height": 384
   },
@@ -79,11 +235,43 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map24_cave": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map24_insides": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map2_cave": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map2_insides": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map2_misc": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map3": {
     "width": 192,
     "height": 192
   },
+  "cont2map3_insides": {
+    "width": 96,
+    "height": 96
+  },
   "cont2map4": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map4_caves": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map4_insides": {
     "width": 384,
     "height": 384
   },
@@ -91,7 +279,31 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map5_caves": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map5_inmsch": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map5_insides": {
+    "width": 768,
+    "height": 768
+  },
+  "cont2map5_underground": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map6": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map6_caves": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map6_insides": {
     "width": 384,
     "height": 384
   },
@@ -99,11 +311,179 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "cont2map7_insides": {
+    "width": 384,
+    "height": 384
+  },
   "cont2map8": {
     "width": 384,
     "height": 384
   },
+  "cont2map8_cave": {
+    "width": 192,
+    "height": 192
+  },
+  "cont2map8_insides": {
+    "width": 192,
+    "height": 192
+  },
   "cont2map9": {
+    "width": 384,
+    "height": 384
+  },
+  "cont2map9_insides": {
+    "width": 96,
+    "height": 96
+  },
+  "cont2map9_temple": {
+    "width": 192,
+    "height": 192
+  },
+  "dream_temple": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_bard": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_boc": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_circle": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_co": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_dao": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_dd": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_doa": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_egow": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_epicevents": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_fd": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_gwar": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_iron": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_ka": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_kd": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_kotp": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_la": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_le": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_lgc": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_lion": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_lll": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_lnx": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_lots": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_nl": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_nu": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_ozu": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_pigs": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_pkg": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_pl": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_riva": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_segv": {
+    "width": 384,
+    "height": 384
+  },
+  "guildmap_situ": {
+    "width": 384,
+    "height": 384
+  },
+  "instance_dungeon": {
+    "width": 384,
+    "height": 384
+  },
+  "instance_ice": {
+    "width": 768,
+    "height": 768
+  },
+  "instance_mountain": {
+    "width": 768,
+    "height": 768
+  },
+  "instance_swamp": {
+    "width": 384,
+    "height": 384
+  },
+  "invasions": {
     "width": 384,
     "height": 384
   },
@@ -111,7 +491,31 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "map11_crypt": {
+    "width": 96,
+    "height": 96
+  },
+  "map11_insides": {
+    "width": 384,
+    "height": 384
+  },
+  "map11_insmsch": {
+    "width": 384,
+    "height": 384
+  },
+  "map11_mschcave": {
+    "width": 96,
+    "height": 96
+  },
+  "map11_mschgar": {
+    "width": 192,
+    "height": 192
+  },
   "map12": {
+    "width": 192,
+    "height": 192
+  },
+  "map12_cave": {
     "width": 192,
     "height": 192
   },
@@ -119,7 +523,19 @@ window.mapSizes = {
     "width": 192,
     "height": 192
   },
+  "map13_castle": {
+    "width": 192,
+    "height": 192
+  },
+  "map13_insides": {
+    "width": 96,
+    "height": 96
+  },
   "map14f": {
+    "width": 192,
+    "height": 192
+  },
+  "map15_cave": {
     "width": 192,
     "height": 192
   },
@@ -131,9 +547,29 @@ window.mapSizes = {
     "width": 768,
     "height": 768
   },
+  "map2_cave": {
+    "width": 96,
+    "height": 96
+  },
+  "map2_insides": {
+    "width": 384,
+    "height": 384
+  },
+  "map2_insidescastle": {
+    "width": 192,
+    "height": 192
+  },
   "map3": {
     "width": 384,
     "height": 384
+  },
+  "map3_insides": {
+    "width": 192,
+    "height": 192
+  },
+  "map4_past": {
+    "width": 192,
+    "height": 192
   },
   "map4f": {
     "width": 192,
@@ -143,7 +579,19 @@ window.mapSizes = {
     "width": 192,
     "height": 192
   },
+  "map5nf_insides": {
+    "width": 192,
+    "height": 192
+  },
   "map6nf": {
+    "width": 384,
+    "height": 384
+  },
+  "map6nf_cave": {
+    "width": 192,
+    "height": 192
+  },
+  "map6nf_insides": {
     "width": 384,
     "height": 384
   },
@@ -151,16 +599,88 @@ window.mapSizes = {
     "width": 384,
     "height": 384
   },
+  "map7_cave": {
+    "width": 192,
+    "height": 192
+  },
+  "map7_insides": {
+    "width": 384,
+    "height": 384
+  },
   "map8": {
     "width": 192,
     "height": 192
+  },
+  "map8_catacombs": {
+    "width": 192,
+    "height": 192
+  },
+  "map8_insides": {
+    "width": 96,
+    "height": 96
   },
   "map9f": {
     "width": 384,
     "height": 384
   },
+  "map9f_insides": {
+    "width": 192,
+    "height": 192
+  },
+  "maparena1": {
+    "width": 96,
+    "height": 96
+  },
+  "mapnewbie": {
+    "width": 192,
+    "height": 192
+  },
+  "mapnewbie_insides": {
+    "width": 768,
+    "height": 768
+  },
+  "mapportalroom": {
+    "width": 96,
+    "height": 96
+  },
+  "mapunderworld1": {
+    "width": 192,
+    "height": 192
+  },
+  "mine1": {
+    "width": 96,
+    "height": 96
+  },
+  "mine1_sub": {
+    "width": 384,
+    "height": 384
+  },
+  "misc1": {
+    "width": 192,
+    "height": 192
+  },
+  "neo": {
+    "width": 96,
+    "height": 96
+  },
+  "newcharactermap": {
+    "width": 192,
+    "height": 192
+  },
+  "nomap": {
+    "width": 768,
+    "height": 768
+  },
   "startmap": {
     "width": 192,
     "height": 192
+  },
+  "startmap_insides": {
+    "width": 96,
+    "height": 96
+  },
+  "testermap": {
+    "width": 768,
+    "height": 768
   }
 };
