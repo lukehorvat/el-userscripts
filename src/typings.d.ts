1bb402c15699101d67b8b4b76c6707b5b2eca969
Luke Horvat <lukehorvat@gmail.com>
2023-07-23 18:07:11 +0200

    Fix typo in typings.d.ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,12 +1,12 @@
 declare module 'decode-dxt' {
-  export type DXTFormat = 'dxt1' | 'dxt2' | 'dxt3' | 'dxt4' | 'dxt4';
+  export type DXTFormat = 'dxt1' | 'dxt2' | 'dxt3' | 'dxt4' | 'dxt5';
 
   export type DecodeDXTFn = {
     (
       imageDataView: DataView,
       width: number,
       height: number,
-      format: DXTFormat
+      format?: DXTFormat
     ): Uint8Array;
   } & {
     [F in DXTFormat]: F;
